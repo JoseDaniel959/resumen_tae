55cede3afbb8938b64f8e5058ec28766b1b49cb8
Daniel-S28 <80227692+Daniel-S28@users.noreply.github.com>
2022-09-05 21:58:55 -0500

    boxplot front

--- main_strealit.py
+++ main_strealit.py
@@ -19,3 +19,21 @@ carreras_show = [key for key, value in carreras.items() if value]
 df = pd.read_csv('ensayo_graficas.csv')
 
 st.pyplot(barplot_cat(df, cluster, carreras_show))
+
+
+"""Gráfica de los boxplot"""
+clusters = {
+     "0": st.checkbox("0", value=True),
+     "1": st.checkbox("1"),
+     "2": st.checkbox("2"),
+     "3": st.checkbox("3"),
+}
+
+continua = st.radio(
+     "Seleccione la variable",
+     ('DEBT_MDN','PCTFLOAN','GRAD_DEBT_MDN','PCIP11','PCIP15','PCIP14','PCIP27','PCTPELL')
+)
+
+clusters_show = [int(key) for key, value in clusters.items() if value]
+
+st.pyplot(boxplot_cluster_vs_continuas(df, clusters_show, continua))
