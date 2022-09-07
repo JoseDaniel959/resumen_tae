8126605ffcb80eecb5daef671dc551e2d227e143
daniel.daza1 <daniel.daza@quipux.com>
2022-09-07 11:11:21 -0500

    feat: se implementa modelo de texto metadata en el tab1

--- metadata.py
+++ metadata.py
@@ -1,15 +1,23 @@
-class metadata:
-
-    caract_per_clust={
-        'clust1':{
-            'grafica1':'Texto grafica1',
-            'grafica2':'Texto grafica2',
-            'grafica3':'Texto grafica3',
+class Texto:
+    caract_per_clust = {
+        0: {
+            'grafica1': 'Texto grafica1 clust1',
+            'grafica2': 'Texto grafica2',
+            'grafica3': 'Texto grafica3',
         },
-        'clust2':{
-            'grafica1':'Texto grafica1',
-            'grafica2':'Texto grafica2',
-            'grafica3':'Texto grafica3',
+        1: {
+            'grafica1': 'Texto grafica1',
+            'grafica2': 'Texto grafica2',
+            'grafica3': 'Texto grafica3',
+        },
+        2: {
+            'grafica1': 'Texto grafica1 clust1',
+            'grafica2': 'Texto grafica2',
+            'grafica3': 'Texto grafica3',
+        },
+        3: {
+            'grafica1': 'Texto grafica1',
+            'grafica2': 'Texto grafica2',
+            'grafica3': 'Texto grafica3',
         }
     }
-    
--- tabs.py
+++ tabs.py
@@ -1,8 +1,11 @@
 import streamlit as st
 from graficos_mtlp import *
+from metadata import Texto
 
 
 def tab_show(cluster, df):
+
+    st.write(Texto.caract_per_clust[cluster]['grafica1'])
     carreras = {
          'CIP11BACHL': st.checkbox('CIP11BACHL' + ' '*cluster, value=True),
          'CIP14BACHL': st.checkbox('CIP14BACHL' + ' '*cluster),
