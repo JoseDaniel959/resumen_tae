94f22e7dfd9c84388833c44aca2e911b8650d009
JoseDaniel959 <j.daniel2000@hotmail.com>
2022-09-08 10:42:54 -0500

    se añade textos

--- tabs.py
+++ tabs.py
@@ -14,3 +14,32 @@ def tab_show(cluster, df):
     }
     carreras_show = [key for key, value in carreras.items() if value]
     st.pyplot(barplot_cat(df, cluster, carreras_show))
+
+def continuas_show(df):
+     options = st.multiselect(
+        '¿Que cluster quieres comparar?',
+        [0, 1, 2, 3],
+        [0, 1, 2, 3])
+    #clusters = [value for value in options.values()]
+     option = st.selectbox(
+        '¿Qué variable continua desea comparar?',
+        ('DEBT_MDN','PCTFLOAN','GRAD_DEBT_MDN','PCIP11','PCIP15','PCIP14','PCIP27','PCTPELL'))
+
+    #print(option)
+
+   
+    
+     st.write(str(options))
+     if(len(options) != 0):
+          
+          st.markdown('### Boxplot de una variable continua vs clusters seleccionados')
+          st.pyplot(boxplot_cluster_vs_continuas(df,options,option))
+          
+          st.markdown('### Diagrama de densidad de una variable continua vs clusters seleccionados')
+          st.pyplot(dens_plot_cluster_vs_continuas(df,options,option))
+
+          st.markdown('### Diagrama de violin de una variable continua vs clusters seleccionados')
+          st.pyplot(violin_plot_cluster_vs_continuas(df,options,option))
+     else:     
+          st.markdown("### Seleccione algún cluster")
+     
